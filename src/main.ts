607593abef2b2fcfa18a57c583066260c6c0a3e8
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2022-08-05 19:11:18 -0400

    fix: port tracking in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
